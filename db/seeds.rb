c3744cfd43f9cb8a02e3e9948d17ad312d34894a
PRADEEPPERIYASAMYTAMU <papradeep11cs@tamu.edu>
2025-09-24 20:21:33 -0500

    feat: added seeds data for movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+more_movies = [
+    {:title => 'My Neighbor Totoro', :rating => 'G',
+      :release_date => '16-Apr-1988'},
+    {:title => 'Green Book', :rating => 'PG-13',
+      :release_date => '16-Nov-2018'},
+    {:title => 'Parasite', :rating => 'R',
+      :release_date => '30-May-2019'},
+    {:title => 'Nomadland', :rating => 'R',
+      :release_date => '19-Feb-2021'},
+    {:title => 'CODA', :rating => 'PG-13',
+      :release_date => '13-Aug-2021'},
+    # New movies added to meet requirements
+    {:title => 'The Godfather', :rating => 'R',
+      :release_date => '24-Mar-1972'},
+    {:title => 'Alien', :rating => 'R',
+      :release_date => '25-May-1979'},
+    {:title => 'Dune', :rating => 'PG-13',
+      :release_date => '22-Oct-2021'}
+  ]
+  
+  more_movies.each do |movie|
+    # Use find_or_create_by to avoid creating duplicates
+    Movie.find_or_create_by!(movie)
+  end
